eb00b9e507e1185f2b978e60d42945ebbacdb6be
David Acosta <dacostacsit@gmail.com>
2023-12-12 13:34:05 -0500

    Add swiftui template project

---
+++ procastiless/procastiless/procastilessApp.swift
@@ -0,0 +1,17 @@
+//
+//  procastilessApp.swift
+//  procastiless
+//
+//  Created by David Acosta on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct procastilessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
